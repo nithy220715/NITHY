77df7b4b46facf0f785f87c66dbbc1ee86e0b8ca
Nithy <nithy220715@gmail.com>
2026-03-20 20:25:32 +0530

    update

--- app/src/main/java/com/example/fintech/MainActivity.kt
+++ app/src/main/java/com/example/fintech/MainActivity.kt
@@ -1,29 +1,15 @@
-package com.example.fintech
 
-import android.annotation.SuppressLint
+package com.example.fintech
 import android.os.Bundle
-import android.webkit.WebChromeClient
 import android.webkit.WebView
-import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
-
-    @SuppressLint("SetJavaScriptEnabled")
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        val webView = WebView(this)
-        setContentView(webView)
-
-        val settings = webView.settings
-        settings.javaScriptEnabled = true
-        settings.domStorageEnabled = true
-        settings.allowFileAccess = true
-        settings.allowContentAccess = true
-
-        webView.webViewClient = WebViewClient()
-        webView.webChromeClient = WebChromeClient()
-
-        webView.loadUrl("file:///android_asset/index.html")
-    }
+class MainActivity: AppCompatActivity(){
+ override fun onCreate(savedInstanceState: Bundle?){
+  super.onCreate(savedInstanceState)
+  val w=WebView(this)
+  setContentView(w)
+  w.settings.javaScriptEnabled=true
+  w.loadUrl("file:///android_asset/index.html")
+ }
 }
